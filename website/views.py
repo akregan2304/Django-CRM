30940fd85cfd9e5625ddc74b50bb33639f99d549
ak2304 <amr2341997>
2024-02-06 13:39:16 +0530

    registration

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.contrib.auth.models import User
+from .forms import SignUpForm
 
 # Create your views here.
 def index(request):
@@ -29,5 +29,18 @@ def logout_user(request):
         return redirect('index')
     
 def register_user(request):
-    return render(request, 'register.html')
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.info(request, "You have successfully registered.Welcome!")
+            return redirect('index')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
         
