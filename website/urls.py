5a17ac58f82ab37f5f740f63aafde0ca96dabc91
ak2304 <amr2341997>
2024-02-08 15:43:45 +0530

    Final Project

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     #path('login/', views.login, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete/<int:pk>', views.delete_record, name='delete'),
+    path('add/', views.add_record, name='add'),
+    path('edit/<int:pk>', views.edit_record, name='edit'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
+from .models import record
 
 # Create your views here.
 def index(request):
+    records = record.objects.all()
+    
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -21,7 +24,7 @@ def index(request):
             return redirect('index')
     
     else:
-        return render(request, 'index.html')
+        return render(request, 'index.html',{'records':records})
     
 def logout_user(request):
         logout(request)
@@ -44,3 +47,48 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form':form})
         
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #look up for records
+        customer_record = record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.info(request, "You must login to see the result...")
+        return redirect('index')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = record.objects.get(id=pk)
+        delete_it.delete()
+        messages.info(request, "You have deleted the data...")
+        return redirect('index')
+    else:
+        messages.info(request, "You must login to see the result...")
+        return redirect('index')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record =form.save()
+                messages.info(request, "Your data saved successfully....")
+                return redirect('index')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.info(request, "You must login to do this task....")
+        return render(request, 'add_record.html', {'customer_record':customer_record})
+    
+
+def edit_record(request, pk):
+    if request.user.is_authenticated:
+        edited = record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=edited)
+        if form.is_valid():
+            form.save()
+            messages.info(request, "Record Has Been Updated!")
+            return redirect('index')
+        return render(request, 'edit.html',{'form':form})
+    else:
+        messages.info(request, "You must login to do this task....")
+        return render('index')
